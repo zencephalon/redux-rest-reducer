e58965cd79ad79d774f7a7e48d902492d11df1de
Matthew Bunday <zenceph@gmail.com>
2016-06-23 11:48:07 -0400

    New babel setup working

---
+++ source/index.js
@@ -0,0 +1,11 @@
+import api from './api'
+import httpReducer from './httpReducer'
+import neoActions from './neoActions'
+import neoActionTypes from './neoActionTypes'
+
+module.exports = {
+  api,
+  httpReducer,
+  neoActions,
+  neoActionTypes,
+}
